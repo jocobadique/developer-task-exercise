8af98227f375b0a554cbc6ee1e6bff2c411e357d
cwdjocobadique <joco.b@cloudwalkdigital.com>
2025-11-21 00:58:51 +0800

    update id

--- backend-django/api/services/datastore.py
+++ backend-django/api/services/datastore.py
@@ -5,7 +5,7 @@ import os
 def get_client():
     # When running locally with emulator:
     # DATASTORE_EMULATOR_HOST and DATASTORE_PROJECT_ID must be set
-    project = os.getenv("DATASTORE_PROJECT_ID", "developer-task-exercise")
+    project = os.getenv("DATASTORE_PROJECT_ID", "fc-itw-joco")
     return datastore.Client(project=project)
 
 client = get_client()
